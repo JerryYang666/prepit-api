204701c13cd3faeca2cc5de3b0e28ad2b6c5646b
Ruihuang Yang <rxy216@case.edu>
2024-04-14 23:41:32 -0400

    fix: type of step

--- common/AgentPromptHandler.py
+++ common/AgentPromptHandler.py
@@ -62,7 +62,7 @@ class AgentPromptHandler:
         logging.info(f"Cache miss, getting the agent prompt from the database. {agent_id}")
         try:
             response = self.table.query(
-                KeyConditionExpression=Key('agent_id').eq(agent_id) & Key('step').eq(step)
+                KeyConditionExpression=Key('agent_id').eq(agent_id) & Key('step').eq(str(step))
             )
             if response['Items']:
                 prompt = response['Items'][0]['prompt']
