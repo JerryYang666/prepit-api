8e00b0b7c8705bc42d310f44e6276679a10eb084
Ruihuang Yang <rxy216@case.edu>
2024-04-16 20:00:47 -0400

    change: use OpenAI latest model

--- user/ChatStream.py
+++ user/ChatStream.py
@@ -116,7 +116,7 @@ class ChatStream:
         :return:
         """
         with self.openai_client.chat.completions.create(
-                model="gpt-4-turbo-preview",
+                model="gpt-4-turbo",
                 messages=messages,
                 stream=True,
                 max_tokens=256,
