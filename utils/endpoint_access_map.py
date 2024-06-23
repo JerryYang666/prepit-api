f3b66c2beae6bb675657224cb1d9ddfcd9047aca
JerryYang666 <53954805+JerryYang666@users.noreply.github.com>
2024-06-23 02:10:34 -0400

    add interview chat history (#5)
    * feat: thread viewing
    
    * add new thread access control
    
    * add new thread access control
    
    * add status to list threads
    
    * fix: user id type
    
    * feat: validate endpoint for edge
    
    * fix: return agent id
    
    * add user id
    
    * add user id
    
    * adjust access, CORS
    
    * adjust data class
    
    * adjust data class

--- utils/endpoint_access_map.py
+++ utils/endpoint_access_map.py
@@ -16,12 +16,11 @@ endpoint_access_map = {
     # above are all admin endpoints
     "/agent/get/{agent_id}": {"student": True, "teacher": True, "admin": True},  # student get agent by id
     "/threads/new_thread": {"student": True, "teacher": True, "admin": True},
-    "/threads/get_thread/{thread_id}": {"student": False, "teacher": True, "admin": True},
+    "/threads/get_thread/{thread_id}": {"student": True, "teacher": True, "admin": True},
     "/threads/get_thread_list": {"student": True, "teacher": True, "admin": True},
     "/stream_chat": {"student": True, "teacher": True, "admin": True},
     "/get_tts_file": {"student": True, "teacher": True, "admin": True},
     "/get_temp_stt_auth_code": {"student": True, "teacher": True, "admin": True},
-    "/get_new_thread": {"student": True, "teacher": True, "admin": True},
     "/access/get_user_list": {"student": False, "teacher": True, "admin": True},
     "/access/grant_access": {"student": False, "teacher": True, "admin": True},
 }
